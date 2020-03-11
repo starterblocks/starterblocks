d9e83a9fa8f0535d5ad3ad905038b66acc39ce65
Dovy Paukstys <me@dovy.io>
2020-03-11 16:29:08 -0400

    Prep for hiding dependencies while loading.

--- src/modal-library/sidebar/index.js
+++ src/modal-library/sidebar/index.js
@@ -10,22 +10,26 @@ import CategoryFilter from './categoryFilter';
 import DependencyFilter from './dependencyFilter';
 
 function Sidebar(props) {
-    const {itemType, layer} = props;
-    const hasSidebar = () => {
-        return (itemType !== 'collection' || layer === null);
-    };
-    return (
-        <div className="starterblocks-modal-sidebar-content">
-            {
-                hasSidebar() &&
-                <div>
-                    <PriceFilter />
-                    <CategoryFilter />
-					<DependencyFilter />
-                </div>
-            }
-        </div>
-    );
+	const {itemType, layer, loading} = props;
+	const hasSidebar = () => {
+		return (itemType !== 'collection' || layer === null);
+	};
+	return (
+		<div className="starterblocks-modal-sidebar-content">
+			{
+				hasSidebar() &&
+				<div>
+					<PriceFilter/>
+					<CategoryFilter/>
+					{/*
+						TODO - Hide if still loading from API
+							{loading && <DependencyFilter/>}
+					*/}
+					<DependencyFilter/>
+				</div>
+			}
+		</div>
+	);
 }
 
 export default withSelect((select, props) => {
