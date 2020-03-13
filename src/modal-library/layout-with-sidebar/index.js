73d90e7bb907310e8248b3a839a9a20c112ae17a
Alfred Takala <alfredtakala@yandex.com>
2020-03-13 11:28:49 +0800

    #18 scroll top when data changes

--- src/modal-library/layout-with-sidebar/index.js
+++ src/modal-library/layout-with-sidebar/index.js
@@ -1,20 +1,33 @@
-const { Component, Fragment } = wp.element;
-
+const { Component, Fragment, useRef, useEffect } = wp.element;
+const { withSelect, select } = wp.data;
 import Sidebar from '../sidebar';
 import TemplateListSubHeader from '~starterblocks/components/template-list-subheader';
 import TemplateList from '../view-template-list';
 
 
-export default function WithSidebarCollection (props) {
+function WithSidebarCollection (props) {
+    const { pageData } = props;
+    const contentAreaRef = useRef(null);
+    useEffect(() => {
+        contentAreaRef.current.scrollTop = 0;
+    }, [pageData])
+
     return (
         <Fragment>
             <div className="starterblocks-collection-modal-sidebar">
                 <Sidebar />
             </div>
-            <div className="starterblocks-collection-modal-content-area">
+            <div className="starterblocks-collection-modal-content-area" ref={contentAreaRef}>
                 <TemplateListSubHeader />
                 <TemplateList />
             </div>
         </Fragment>
     );
 }
+
+
+export default
+    withSelect((select, props) => {
+        const { getPageData } = select('starterblocks/sectionslist');
+        return { pageData: getPageData() };
+    })(WithSidebarCollection);
