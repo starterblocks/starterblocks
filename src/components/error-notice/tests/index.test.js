5e23a734991df4b2cb3c7597f7a3f33b64e859f6
Alfred Takala <alfredtakala@yandex.com>
2020-03-20 12:37:40 +0800

    simple tests for error notice: p tag & prop data type

--- src/components/error-notice/tests/index.test.js
+++ src/components/error-notice/tests/index.test.js
@@ -1,11 +1,31 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import {ErrorNotice} from '../';
 
 describe('Error Notice', () => {
+    // Snapshot testing
     it('renders correctly', () => {
         const onButtonClickMock = jest.fn();
         const wrapper = shallow(<ErrorNotice errorMessages={['Some Error Message']} discardAllErrorMessages={onButtonClickMock} />);
         expect(wrapper).toMatchSnapshot();
-        // On the first run of this test, Jest will generate a snapshot file automatically.
+    });
+
+    it('has always one p tag', () => {
+        const props = {
+                errorMessages: [],
+                discardAllErrorMessages: jest.fn()
+            },
+            ErrorNoticeComponent = mount(<ErrorNotice {...props} />);
+        expect(ErrorNoticeComponent.find('p').length).toBe(1);
+    });
+
+    // prop data type testing
+    it('check the type of props', () => {
+        const props = {
+                errorMessages: [],
+                discardAllErrorMessages: jest.fn()
+            },
+            ErrorNoticeComponent = mount(<ErrorNotice {...props} />);
+        expect(Array.isArray(ErrorNoticeComponent.prop('errorMessages'))).toBe(true);
+        expect(typeof ErrorNoticeComponent.prop('discardAllErrorMessages')).toBe('function');
     });
 });
