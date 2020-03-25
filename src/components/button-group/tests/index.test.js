8758833bf3f94d94c76e57eab180f0613b6f5a5f
Alfred Takala <alfredtakala@yandex.com>
2020-03-25 11:40:04 +0800

    jest better structure and code refactor

--- src/components/button-group/tests/index.test.js
+++ src/components/button-group/tests/index.test.js
@@ -1,10 +1,7 @@
-import { shallow, mount } from 'enzyme';
-import { render } from '@testing-library/react'
-
-import ButtonGroup from '../';
-import {TemplateModalProvider} from '../../../contexts/TemplateModalContext';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import renderer from 'react-test-renderer';
+import ButtonGroup from '../';
+import {TemplateModalProvider} from '../../../contexts/TemplateModalContext';
 
 // To be used fro snapshot
 const TestComponent = (props) => {
@@ -24,48 +21,39 @@ let realUseContext, useContextMock;
 let useEffect;
 
 describe('Button Group', () => {
-    const mockUseEffect = () => {
-        useEffect.mockImplementationOnce(f => f());
-    };
-
-    beforeEach(() => {
-        useEffect = jest.spyOn(React, 'useEffect');
-    });
-
-    afterEach(() => {
-
-    });
-
-    // 1. Snapshot testing
-    it('renders correctly', () => {
-        // const element = shallow(<TestComponent spinner={null} />);
+    it('1. renders correctly: snapshot testing', () => {
         const component = renderer.create(<TestComponent spinner={null} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
-        // expect(element).toMatchSnapshot();
     });
 
-    // 2. Testing Props: default Prop
-    it('renders the default classname with spinner null', () => {
-        realUseContext = React.useContext;
-        useContextMock = React.useContext = jest.fn();
-        useContextMock.mockReturnValue({spinner: null});
-        const renderer = new ShallowRenderer();
-        const element = renderer.render(<ButtonGroup />);
-        expect(element.props.className).toBe('starterblocks-import-button-group');
-        React.useContext = realUseContext;
-    })
+    describe('2. Testing props with react hook!!!', () => {
+        const mockUseEffect = () => {
+            useEffect.mockImplementationOnce(f => f());
+        };
 
-    it('renders disabled status with spinner not null', () => {
-        realUseContext = React.useContext;
-        useContextMock = React.useContext = jest.fn();
-        mockUseEffect();
-        useContextMock.mockReturnValue({spinner: 1});
-        const element = new ShallowRenderer().render(<ButtonGroup />);
-        expect(element.props.className).toBe('starterblocks-import-button-group disabled');
-        React.useContext = realUseContext;
-    })
+        beforeEach(() => {
+            useEffect = jest.spyOn(React, 'useEffect');
+            realUseContext = React.useContext;
+            useContextMock = React.useContext = jest.fn();
+        });
 
-    // 3. Data types:
+        afterEach(() => {
+            React.useContext = realUseContext;
+        });
+
+        it('renders the default classname with spinner null', () => {
+            useContextMock.mockReturnValue({spinner: null});
+            const element = new ShallowRenderer().render(<ButtonGroup />);
+            expect(element.props.className).toBe('starterblocks-import-button-group');
+        })
+
+        it('renders disabled status with spinner not null', () => {
+            mockUseEffect();
+            useContextMock.mockReturnValue({spinner: 1});
+            const element = new ShallowRenderer().render(<ButtonGroup />);
+            expect(element.props.className).toBe('starterblocks-import-button-group disabled');
+        })
+    })
 
 });
