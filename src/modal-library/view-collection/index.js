f469fc67f7ae933410803b6e466bbe9f33c8e49e
Alfred Takala <alfredtakala@yandex.com>
2020-04-13 21:18:43 +0800

    section title based on navigating within collection

--- src/modal-library/view-collection/index.js
+++ src/modal-library/view-collection/index.js
@@ -1,4 +1,4 @@
-const {useState} = wp.element;
+const {useState, useEffect} = wp.element;
 const {compose} = wp.compose;
 const {withDispatch, withSelect, select} = wp.data;
 const {__} = wp.i18n
@@ -11,60 +11,63 @@ import {requiresInstall, requiresPro} from '~starterblocks/stores/dependencyHelp
 function CollectionView(props) {
     const {pageData, loading, activeCollection, activeItemType} = props;
     const {setActiveCollection} = props;
-
+    const [previewData, setPreviewData] = useState(null);
     const [previewDataIndex, setPreviewDataIndex] = useState(0);
 
     const dataLength = pageData.length;
 
-    let previewData = pageData[previewDataIndex];
+    useEffect(() => {
+        if (pageData) setPreviewData(pageData[previewDataIndex]);
+    }, [pageData, previewDataIndex]);
 
 
+    if (previewData)
+        return (
+            <div className="starterblocks-collection-details-view">
+                <div className="starterblocks-collection-details-left">
+                    <div className="details-back" onClick={() => setActiveCollection(null)}>
+                        <span className="dashicons dashicons-arrow-left-alt"/>&nbsp;Back to Collections
+                    </div>
+                    <div className="details-preview" style={{backgroundImage: `url('${previewData.image}')`}}>
 
-    return (
-        <div className="starterblocks-collection-details-view">
-            <div className="starterblocks-collection-details-left">
-                <div className="details-back" onClick={() => setActiveCollection(null)}>
-                    <span className="dashicons dashicons-arrow-left-alt"/>&nbsp;Back to Collections
+                    </div>
                 </div>
-                <div className="details-preview" style={{backgroundImage: `url('${previewData.image}')`}}>
+                <div className="starterblocks-collection-details-right">
+                    <div className="details-title">
+                        <h3>{previewData.name}</h3>
+                        <span>{dataLength} pages</span>
+                    </div>
+                    <div className="details-list">
+                        <div className="details-inner">
+                            {
+                                pageData.map((detail, index) => {
+                                    let className = (previewData.ID === detail.ID) ? 'detail-select detail-select-active' : 'detail-select';
+                                    let divStyle = {
+                                        backgroundImage: 'url(' + detail.image + ')',
+                                    };
 
-                </div>
-            </div>
-            <div className="starterblocks-collection-details-right">
-                <div className="details-title">
-                    <h3>Section Title</h3>
-                    <span>{dataLength} pages</span>
-                </div>
-                <div className="details-list">
-                    <div className="details-inner">
-                        {
-                            pageData.map((detail, index) => {
-                                let className = (previewData.ID === detail.ID) ? 'detail-select detail-select-active' : 'detail-select';
-                                let divStyle = {
-                                    backgroundImage: 'url(' + detail.image + ')',
-                                };
-
-                                return (
-                                    <div className={className} onClick={() => setPreviewDataIndex(index)} key={index}>
-                                        <div className="detail-image" style={divStyle}>
-                                            {requiresPro(detail) && <span className="pro">{__('Premium')}</span>}
-                                            {!requiresPro(detail) && requiresInstall(detail) && <span className="install"><i className="fas fa-exclamation-triangle" /></span>}
-                                            <div className="detail-label">{detail.name}</div>
+                                    return (
+                                        <div className={className} onClick={() => setPreviewDataIndex(index)} key={index}>
+                                            <div className="detail-image" style={divStyle}>
+                                                {requiresPro(detail) && <span className="pro">{__('Premium')}</span>}
+                                                {!requiresPro(detail) && requiresInstall(detail) && <span className="install"><i className="fas fa-exclamation-triangle" /></span>}
+                                                <div className="detail-label">{detail.name}</div>
+                                            </div>
                                         </div>
-                                    </div>
-                                );
-                            })
-                        }
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="starterblocks-collection-details-footer">
+                    <div className="footer-grid">
+                        <ButtonGroup index={previewDataIndex} showDependencyBlock={false} data={previewData} pageData={pageData} />
                     </div>
                 </div>
             </div>
-            <div className="starterblocks-collection-details-footer">
-                <div className="footer-grid">
-                    <ButtonGroup index={previewDataIndex} showDependencyBlock={false} data={previewData} pageData={pageData} />
-                </div>
-            </div>
-        </div>
-    );
+        );
+    return null;
 }
 
 
