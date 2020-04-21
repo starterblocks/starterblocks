22a9c918aa8a3bac53e311948f10279510fe3a8e
Alfred Takala <alfredtakala@yandex.com>
2020-04-21 13:49:30 +0800

    #67 disabling side panel selection

--- src/modal-library/sidebar/dependencyFilter.js
+++ src/modal-library/sidebar/dependencyFilter.js
@@ -1,13 +1,11 @@
-import React from 'react';
-
 const {Component, Fragment} = wp.element;
 const {compose} = wp.compose;
 const {select, withDispatch, withSelect} = wp.data;
 const {__} = wp.i18n;
 
 import {CheckboxControl, Tooltip} from '@wordpress/components';
-import {pluginInfo} from '~starterblocks/stores/dependencyHelper';
-import groupBy from 'lodash/groupBy';
+import DependencyFilterRow from './dependencyFilterRow';
+
 
 function DependencyFilter(props) {
     const {dependencyFilters, loading, plugins} = props;
@@ -18,28 +16,16 @@ function DependencyFilter(props) {
         setActiveCategory(data.slug);
     };
     // Give the selected category(activeCategory) label className as "active"
-    const isChecked = (pluginKey) => {
-        if (dependencyFilters.hasOwnProperty(pluginKey))
-            return dependencyFilters[pluginKey].hasOwnProperty('value') ? dependencyFilters[pluginKey].value : dependencyFilters[pluginKey];
+    const isNoneChecked = () => {
+        if (dependencyFilters.hasOwnProperty('none'))
+            return dependencyFilters['none'].hasOwnProperty('value') ? dependencyFilters['none'].value : dependencyFilters['none'];
         return false;
     };
 
-    const toggleChecked = (pluginKey) => {
-        if (pluginKey === 'none') {
-            setDependencyFilters({...dependencyFilters,
-                [pluginKey]: { value: dependencyFilters[pluginKey].value === false, disabled: dependencyFilters[pluginKey]['disabled'] === true }});
-        } else {
-            let newDependencyFilters = {...dependencyFilters,
-                [pluginKey]: { value: dependencyFilters[pluginKey].value === false, disabled: dependencyFilters[pluginKey]['disabled'] === true }};
-
-            let valueCount = groupBy(Object.keys(newDependencyFilters), key => (newDependencyFilters[key] === true || newDependencyFilters[key].value === true));
-
-            if (valueCount['true'] && valueCount['true'].length > 0 && valueCount['false'] && valueCount['false'].length > 0) {
-                setDependencyFilters({...newDependencyFilters, none: {value: false, disabled: newDependencyFilters['none']['disabled']}});
-            } else {
-                setDependencyFilters({...newDependencyFilters, none: {value: true, disabled: newDependencyFilters['none']['disabled']}});
-            }
-        }
+    const toggleNoneChecked = (pluginKey) => {
+        setDependencyFilters({...dependencyFilters,
+            none: { value: dependencyFilters['none'].value === false, disabled: dependencyFilters['none']['disabled'] === true }
+        });
     };
 
     const setAllCheckedAs = (newVal) => {
@@ -71,9 +57,9 @@ function DependencyFilter(props) {
                                 {/*>*/}
                                 <CheckboxControl
                                     label={__('Native', 'starterblocks')}
-                                    checked={isChecked('none')}
-                                    disabled={isChecked('none')}
-                                    onChange={() => toggleChecked('none')}
+                                    checked={isNoneChecked()}
+                                    disabled={isNoneChecked()}
+                                    onChange={() => toggleNoneChecked('none')}
                                 />
                                 <Tooltip text={__('Only default WordPress blocks used.', 'starterblocks')} position='right'>
                                     <span style={{float:'right', marginRight:'2px'}}><i className="fa fa-info-circle" /></span>
@@ -82,28 +68,9 @@ function DependencyFilter(props) {
                             </li>
                         }
                         {
-                            Object.keys(dependencyFilters).sort().map(pluginKey => {
-                                if (pluginKey === 'none') return null;
-                                let pluginInstance = pluginInfo(pluginKey)
-                                if (pluginInstance.name == null) {
-                                    return // Skip extra items
-                                }
-                                return (
-                                    <li className={!pluginInstance.version ? 'missing-dependency' : ''} key={pluginKey}>
-                                        <CheckboxControl
-                                            label={pluginInstance.name}
-                                            checked={isChecked(pluginKey)}
-                                            onChange={() => toggleChecked(pluginKey)}
-                                        />
-
-                                        {pluginInstance.url ?
-                                            <a href={pluginInstance.url} target="_blank">
-                                                <i className="fa fa-external-link-alt" />
-                                            </a> : null}
-                                    </li>
-                                );
-
-                            })
+                            Object.keys(dependencyFilters).sort().map(pluginKey =>
+                                <DependencyFilterRow key={pluginKey} pluginKey={pluginKey} />
+                            )
                         }
                     </ul>
                 </div>
