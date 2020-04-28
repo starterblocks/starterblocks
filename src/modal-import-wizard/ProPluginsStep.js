4c9981a889026c226e78447a76c62c6f3e379d27
Alfred Takala <alfredtakala@yandex.com>
2020-04-28 19:02:28 +0800

    styling starterblocks premium box

--- src/modal-import-wizard/ProPluginsStep.js
+++ src/modal-import-wizard/ProPluginsStep.js
@@ -11,28 +11,27 @@ function ProPluginStep(props) {
     const {missingPros, onCloseWizard} = props;
     const {plugins} = props;
 
+    if ( missingPros.indexOf(STARTERBLOCKS_PRO_KEY) >= 0 ) return <StarterBlocksPremiumBox />
     return (
         <Fragment>
             <div className="starterblocks-modal-body">
                 <h5>{__('External Dependencies Required')}</h5>
                 <p>{__('The following premium plugin(s) are required to import this template:')}</p>
-                {
-                    (missingPros.indexOf(STARTERBLOCKS_PRO_KEY) >= 0 ) ?
-                    <StarterBlocksPremiumBox /> :
-                    <ul className="starterblocks-import-progress">
-                        {
-                            missingPros.map(pluginKey => {
-                                let plugin = pluginInfo(pluginKey)
-                                return (
-                                    <li className='installing' key={pluginKey}>
-                                        {plugin.name} {plugin.url &&
-                                    <a href={plugin.url} target="_blank"><i className="fa fa-external-link-alt"/></a>
-                                    }
-                                    </li>);
-                            })
-                        }
-                    </ul>
-                }
+
+                <ul className="starterblocks-import-progress">
+                    {
+                        missingPros.map(pluginKey => {
+                            let plugin = pluginInfo(pluginKey)
+                            return (
+                                <li className='installing' key={pluginKey}>
+                                    {plugin.name} {plugin.url &&
+                                <a href={plugin.url} target="_blank"><i className="fa fa-external-link-alt"/></a>
+                                }
+                                </li>);
+                        })
+                    }
+                </ul>
+
             </div>
             <div className="starterblocks-modal-footer">
                 <a className="button button-secondary" onClick={onCloseWizard}>
