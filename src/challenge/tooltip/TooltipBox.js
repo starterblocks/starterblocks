d94ba374a33dca6c0807440ed60b8c4075f9560e
Alfred Takala <alfredtakala@yandex.com>
2020-05-15 09:38:17 +0800

    arrow positioning

--- src/challenge/tooltip/TooltipBox.js
+++ src/challenge/tooltip/TooltipBox.js
@@ -3,11 +3,14 @@ const { withDispatch, withSelect } = wp.data;
 const { useState, useEffect } = wp.element;
 import CONFIG from '../config';
 const PADDING_X = 20;
-const PADDING_Y = 20;
+const PADDING_Y = 0;
+const ARROW_HEIGHT = 20;
 function TooltipBox(props) {
     const { challengeStep, tooltipRect, isOpen } = props;
     const [style, setStyle] = useState({});
+    const [arrowStyle, setArrowStyle] = useState({});
     const [content, setContent] = useState('');
+    const [wrapperClassname, setWrapperClassname] = useState('');
 
     const isVisible = () => {
         return ((challengeStep >= 0 || challengeStep > CONFIG.totalStep) && isOpen);
@@ -28,6 +31,7 @@ function TooltipBox(props) {
                 return (tooltipRect.left + stepInformation.offset.x);
         } 
     }
+    // adjust position and content upon steps change
     useEffect(() => {
         if (isVisible() && tooltipRect) {
             const stepInformation = CONFIG.list[challengeStep];
@@ -37,19 +41,49 @@ function TooltipBox(props) {
                     display: 'block',
                     width: stepInformation.box.width,
                     left: calculateLeftWithStepInformation(),
-                    top: tooltipRect.top + stepInformation.offset.y
+                    top: tooltipRect.top + stepInformation.offset.y + PADDING_Y + ARROW_HEIGHT
                 });
                 setContent(stepInformation.content);
+                setArrowStyle({
+                    left: tooltipRect.left + stepInformation.offset.x,
+                    top: tooltipRect.top + stepInformation.offset.y + PADDING_Y
+                });
             }
         } else
             setStyle({ ...style, display: 'none' });
     }, [JSON.stringify(tooltipRect), challengeStep, isOpen]);
 
+    // update wrapper class name based on step change
+    useEffect(() => {
+        const stepInformation = CONFIG.list[challengeStep];
+        if (stepInformation) {
+            switch(stepInformation.direction) {
+                case 'top':
+                    setWrapperClassname('challenge-tooltip tooltipster-sidetip tooltipster-top');
+                    break;
+                case 'bottom':
+                    setWrapperClassname('challenge-tooltip tooltipster-sidetip tooltipster-bottom');
+                    break;
+                default:
+                    setWrapperClassname('challenge-tooltip tooltipster-sidetip');
+            }
+            
+        }
+    }, [challengeStep])
+
     return (
-        <div className="tooltip-box" style={style}>
-            {content}
-            <div className="btn-row">
-                <button className="btn">Done</button>
+        <div className={wrapperClassname}>
+            <div className="tooltipster-box" style={style}>
+                {content}
+                <div className="btn-row">
+                    <button className="challenge-done-btn">Done</button>
+                </div>
+            </div>
+            <div class="tooltipster-arrow" style={arrowStyle}>
+                <div class="tooltipster-arrow-uncropped">
+                    <div class="tooltipster-arrow-border"></div>
+                    <div class="tooltipster-arrow-background"></div>
+                </div>
             </div>
         </div>
     );
